32c45e1dfe78364577c4c368dd90b893d5aba2aa
suriya <suriyaboss1@gmail.com>
2023-11-04 07:24:16 +0000

    add

--- index.js
+++ index.js
@@ -10,31 +10,29 @@ const dbPath = path.join(__dirname, "goodreads.db");
 let db = null;
 
 const initializeDBAndServer = async () => {
-    try {
-        db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database,
-        });
-        app.listen(3000, () => {
-            console.log("Server Running at http://localhost:3000/");
-        });
-    } catch (e) {
-        console.log(`DB Error: ${e.message}`);
-        process.exit(1);
-    }
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running... at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
 };
-
-
 
 initializeDBAndServer();
 app.get("/books/", async (request, response) => {
-    const getBooksQuery = `
+  const getBooksQuery = `
     SELECT
       *
     FROM
       book
     ORDER BY
       book_id;`;
-    const booksArray = await db.all(getBooksQuery);
-    response.send(booksArray);
-})
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
