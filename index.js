683dc0672453cb09f743f64951afbd075ee57e6e
suriya <suriyaboss1@gmail.com>
2023-11-06 05:08:20 +0000

    add

--- index.js
+++ index.js
@@ -29,16 +29,25 @@ initializeDBAndServer();
 
 // Get Books API
 app.get("/books/", async (request, response) => {
+  const {
+    offset = 2,
+    limit = 5,
+    order = "ASC",
+    order_by = "book_id",
+    search_q = "",
+  } = request.query;
   const getBooksQuery = `
     SELECT
       *
     FROM
-      book
-    ORDER BY
-      book_id;`;
+     book
+    WHERE
+     title LIKE '%${search_q}%'
+    ORDER BY ${order_by} ${order}
+    LIMIT ${limit} OFFSET ${offset};`;
   const booksArray = await db.all(getBooksQuery);
   response.send(booksArray);
-});
+});});
 app.get("/books/:bookId/", async (request, response) => {
   const { bookId } = request.params;
   const getBookQuery = `
